604b6a365c79c36482ef7428bb7527327f812538
Stephanie Savoie <stephsavoie4@gmail.com>
2017-10-31 10:25:34 -0400

    created tasks controller with action framework and associated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/edit'
+
+  get 'tasks/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 end
