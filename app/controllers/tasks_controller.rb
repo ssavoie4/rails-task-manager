5b8a8ed602a90b7eb11065c9efe5fc881f1d9b60
Stephanie Savoie <stephsavoie4@gmail.com>
2017-10-31 11:18:22 -0400

    completed show index and edit action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,40 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit]
+
   def index
+    @tasks = Task.all
   end
 
   def show
   end
 
-  def create
+  def new
+    @task = Task.new
   end
 
-  def new
+  def create
+    @task = Task.create(task_params)
+
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
   end
 
   def destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :done)
   end
 
 end
